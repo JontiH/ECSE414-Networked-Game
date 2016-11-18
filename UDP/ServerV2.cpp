798d7054a1d7b3692fc014fd1d6245dfc52b9f65
AndresFelipeRinconGamboa <andresfeliperincongamboa@Andress-MacBook-Pro.local>
2016-11-18 12:31:12 -0500

    Adding new server main file
    This is the main file of the server. Server.cpp is for debugging.

---
+++ UDP/ServerV2.cpp
@@ -0,0 +1,10 @@
+//
+//  ServerV2.cpp
+//  
+//
+//  Created by Andres Felipe Rincon Gamboa on 2016-11-18.
+//
+//
+
+#include "ServerV2.hpp"
+#include "UDPSystem.hpp"
---
+++ UDP/ServerV2.hpp
@@ -0,0 +1,14 @@
+//
+//  ServerV2.hpp
+//  
+//
+//  Created by Andres Felipe Rincon Gamboa on 2016-11-18.
+//
+//
+
+#ifndef ServerV2_hpp
+#define ServerV2_hpp
+
+#include <stdio.h>
+
+#endif /* ServerV2_hpp */
